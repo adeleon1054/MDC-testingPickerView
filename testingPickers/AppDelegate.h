72b062f86d104f4fe68543fc3d8e88b6cb823f5f
Asdruval De Leon <adeleon1054@gmail.com>
2017-10-12 18:14:27 -0400

    Initial Commit

---
+++ testingPickers/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  testingPickers
+//
+//  Created by Asdruval De Leon on 10/12/17.
+//  Copyright © 2017 Asdruval De Leon. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ testingPickers/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  testingPickers
+//
+//  Created by Asdruval De Leon on 10/12/17.
+//  Copyright © 2017 Asdruval De Leon. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
